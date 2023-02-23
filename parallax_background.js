87cb5cd1e4174cd532e9ff232994ba3a85a5aaac
GoldieSloth <the_golden_sloth@t-online.de>
2023-02-23 08:24:34 +0100

    Update parallax_background.js

---
+++ parallax_background.js
@@ -0,0 +1,17 @@
+const background = document.querySelector('.background');
+const layers = Array.from(background.querySelectorAll('.layer'));
+
+background.addEventListener('mousemove', e => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  const centerX = window.innerWidth / 2;
+  const centerY = window.innerHeight / 2;
+
+  layers.forEach((layer, index) => {
+    const speed = index / 5;
+    const x = (mouseX - centerX) * speed;
+    const y = (mouseY - centerY) * speed;
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+});
